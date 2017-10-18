8773659527b972230e021293477944f2334c9a65
Ezekiel Trinidad <gamin@LAPTOP-JA5V9VNG.Home>
2017-10-18 19:20:57 +0100

    Implemented CombatHandler.java and Player.java
    1. Added the constructor for both classes
    2. Added fight() for CombatHandler

--- CS2010/src/peril/CombatHandler.java
+++ CS2010/src/peril/CombatHandler.java
@@ -1,10 +1,46 @@
 package peril;
 
+import java.util.Random;
+
+import peril.board.Army;
+import peril.board.Country;
+
 /**
  * Encapsulates all the game combat logic.
- * @author Joshua
+ * 
+ * @author Joshua_Eddy
+ * @author Ezekiel_Trinidad
  *
  */
 public class CombatHandler {
+
+	/**
+	 * {@link Random} object for the random number generator
+	 * 
+	 */
+	private Random random;
+
+	/**
+	 * Constructs a new {@link CombatHandler}.
+	 * 
+	 */
+	public CombatHandler() {
+		random = new Random();
+	}
+
+	/**
+	 * When a {@link Player} clicks on another {@link Country} to fight. This pits 2
+	 * {@link Army}'s against each other. This emulates 1 turn of combat between 2 players.
+	 * 
+	 * @param armyAtk
+	 *            This is the {@link Army} that the {@link Player} uses to attack a
+	 *            {@link Country}.
+	 * @param armydef
+	 *            This is the {@link Army} that defend against the {@link Player}'s
+	 *            attacking {@link Army}.
+	 */
+	public void fight(Army armyAtk, Army armydef) {
+
+	}
 
 }
--- CS2010/src/peril/Player.java
+++ CS2010/src/peril/Player.java
@@ -6,8 +6,28 @@ package peril;
  * that user has.
  * 
  * @author Joshua_Eddy
+ * @author Ezekiel_Trinidad
  *
  */
-public class Player {
-
+public enum Player {
+	PLAYER_ONE("Player 1"), PLAYER_TWO("Player 2"), PLAYER_THREE("Player 3"), PLAYER_FOUR("Player 4");
+	
+	/**
+	 * String Representation of the {@link Player}.
+	 */
+	private final String name;
+	
+	/**
+	 * Constructs a new {@link Player}.
+	 * 
+	 * @param name String Representation of the {@link Player}.
+	 */
+	private Player(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }

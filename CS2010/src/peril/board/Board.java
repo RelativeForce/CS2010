f857179f70f7055e01be83ff50d461458afdfa82
RelativeForce <joshiceddy@hotmail.com>
2017-10-20 10:00:36 +0100

    FIXED MERGE ERROR Board.java

--- CS2010/src/peril/board/Board.java
+++ CS2010/src/peril/board/Board.java
@@ -1,7 +1,10 @@
 package peril.board;
 
 import java.nio.channels.NetworkChannel;
+import java.util.LinkedList;
+import java.util.List;
 
+import peril.Point;
 import peril.ui.Viewable;
 import peril.ui.VisualRepresentation;
 
@@ -16,6 +19,11 @@ import peril.ui.VisualRepresentation;
 public final class Board implements Viewable {
 
 	/**
+	 * The {@link Continent}s in this {@link Board}.
+	 */
+	private List<Continent> continents;
+	
+	/**
 	 * Holds the {@link VisualRepresentation} of the {@link Board}.
 	 */
 	private Visual visual;
@@ -25,6 +33,34 @@ public final class Board implements Viewable {
 	 */
 	private Board(){
 		visual = new Visual();
+		continents = new LinkedList<>();
+	}
+	
+	/**
+	 * Retrieves a {@link Country} that is specified by the parameter {@link Point}.
+	 * 
+	 * @param click
+	 *            {@link Point} on the board.
+	 * @return {@link Country}.
+	 */
+	private Country getCountry(Point click) {
+
+		// TODO: Add the implementation that searches through all the continents regions
+		// and if the the point is inside the region then search all the countries in
+		// that continent.
+
+		// Iterate through all the continents on the board.
+		for (Continent continent : continents) {
+
+			// If the click is inside the continents region get the country from the region.
+			if (continent.getVisual().isClicked(click)) {
+				return continent.getCountryByClick(click);
+			}
+		}
+
+		// If the point is not on the board return null.
+		return null;
+
 	}
 	
 	/**

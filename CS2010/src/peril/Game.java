8b3293faeaa4ad098fa4dd946af04ef61548f17f
RelativeForce <joshiceddy@hotmail.com>
2017-10-24 00:10:37 +0100

    Thread Safety 101
    1. Added a thread safe action transfer object - ProcessTransfer.java.
    2. Finished Game implementation

--- CS2010/src/peril/Game.java
+++ CS2010/src/peril/Game.java
@@ -8,6 +8,8 @@ import java.util.concurrent.LinkedTransferQueue;
 
 import peril.board.Board;
 import peril.io.MapReader;
+import peril.multiThread.Action;
+import peril.multiThread.ProcessTransfer;
 import peril.io.ChallengeReader;
 import peril.ui.UserInterface;
 
@@ -20,19 +22,19 @@ import peril.ui.UserInterface;
 public class Game {
 
 	/**
-	 * Whether or not the background thread is running or not.
+	 * Whether the game is running or not.
 	 */
 	public volatile boolean run;
 
 	/**
-	 * This {@link Queue} holds instructions stored as objects for the background
-	 * thread to execute. This allows for an asynchronous execution resulting is a
-	 * responsive {@link UserInterface}.
-	 * 
-	 * @see Queue
-	 * @see LinkedTransferQueue
+	 * Whether or not the current users turn is over or not.
 	 */
-	private Queue<Action<?>> buffer;
+	public volatile boolean endTurn;
+
+	/**
+	 * {@link ProcessTransfer#getInstane()}
+	 */
+	private ProcessTransfer processTransfer;
 
 	/**
 	 * The instance of the {@link Board} used for this game.
@@ -57,23 +59,32 @@ public class Game {
 	/**
 	 * Contains all the objectives that a {@link Player} can attain in the game.
 	 */
-	private List<Challenge> objectives;
+	private List<Challenge> challenges;
+
+	/**
+	 * The {@link UserInterface} for the {@link Game}.
+	 */
+	private UserInterface ui;
 
 	/**
 	 * Constructs a new {@link Game}.
+	 * 
+	 * @param ui
+	 *            The {@link UserInterface} of the {@link Game}.
 	 */
 	private Game() {
 
 		this.currentPlayerIndex = 0;
 		this.players = Player.values();
 		this.currentRound = 0;
-		this.buffer = new LinkedTransferQueue<>();
+		this.processTransfer = ProcessTransfer.getInstane();
+		this.endTurn = false;
 		this.run = true;
 
 		// Read the Board and Objectives from the files.
 		File currentDirectory = new File(System.getProperty("user.dir"));
 		this.board = MapReader.getBoard(currentDirectory.getPath(), "Earth");
-		this.objectives = ChallengeReader.getChallenges(currentDirectory.getPath(), "Earth");
+		this.challenges = ChallengeReader.getChallenges(currentDirectory.getPath(), "Earth");
 
 	}
 
@@ -105,39 +116,40 @@ public class Game {
 	}
 
 	/**
-	 * Adds a {@link Action} to the background thread queue.
-	 * 
-	 * @param action
-	 *            {@link Action} to perform.
+	 * Executes the core game play loop.
 	 */
-	public void addAction(Action<?> action) {
+	public void play() {
 
-		// Null check.
-		if (action == null) {
-			throw new IllegalArgumentException("Action cannot be null.");
+		ui = new UserInterface(this);
+
+		Player currentPlayer = players[currentPlayerIndex];
+		// While the game is being played.
+		while (run) {
+
+			currentPlayer = players[currentPlayerIndex];
+
+			displayTurn(currentPlayer);
+
+			executeTurn(currentPlayer);
+
+			// Go to next player.
+			nextPlayer();
+
+			// If the last player just had their turn.
+			if (currentPlayerIndex == 0) {
+				endRound();
+			}
 		}
-
-		buffer.add(action);
 
 	}
 
 	/**
-	 * Starts the background {@link Game} loop that will end when {@link Game#run}
-	 * is <code>false</code>.
+	 * Performs the actions of the background loop. Any time the game thread is
+	 * waiting for an for an {@link Action} to execute or to be executed, call this
+	 * method.
 	 */
 	private void run() {
-
-		// Background thread loop.
-		while (run) {
-
-			// If there is an item in the buffer.
-			if (!buffer.isEmpty()) {
-				buffer.poll().execute();
-			}
-
-			// TODO Run music and check for inactivity
-
-		}
+		// TODO Background stuff.
 	}
 
 	/**
@@ -153,33 +165,6 @@ public class Game {
 	}
 
 	/**
-	 * Executes a turn for the {@link Player} specified by
-	 * {@link Game#currentPlayerIndex}.
-	 * 
-	 * @param player
-	 *            {@link Player}.
-	 */
-	public void executeTurn() {
-
-		Player currentPlayer = players[currentPlayerIndex];
-
-		// TODO turn operations.
-
-		// Iterate though all the objectives to see if the the current player has
-		// completed them.
-		objectives.forEach(objective -> objective.hasCompleted(currentPlayer));
-
-		nextPlayer();
-
-		/**
-		 * If the last player just had their turn.
-		 */
-		if (currentPlayerIndex == 0) {
-			endRound();
-		}
-	}
-
-	/**
 	 * Parses a key press on the {@link Game#board}.
 	 * 
 	 * @param code
@@ -191,11 +176,112 @@ public class Game {
 		// TODO Parse input
 	}
 
+	/**
+	 * Creates an {@link Action} that displays the specified winning {@link Player}
+	 * on the {@link UserInterface}. This {@link Action} is then passed to the
+	 * {@link UserInterface} thread while this thread waits for it to be completed
+	 * using {@link Action#isDone()}.
+	 * 
+	 * @param player
+	 *            {@link Player}
+	 */
+	private void displayWinner(Player player) {
+
+		// Display the winner to the user.
+		Action<UserInterface> action = new Action<>(ui, ui -> ui.displayWinner(player));
+
+		// Transfer the action to the ui.
+		processTransfer.transfer(action);
+
+		// Wait for winner to be displayed.
+		while (!action.isDone()) {
+			run();
+		}
+
+	}
+
+	/**
+	 * Executes a turn for a specified {@link Player}.
+	 * 
+	 * @param player
+	 *            {@link Player}.
+	 */
+	private void executeTurn(Player player) {
+
+		// While the user has his turn.
+		while (!endTurn && run) {
+
+			while (processTransfer.isEmpty() && run) {
+				// Wait for the player to perform an action.
+				run();
+			}
+
+			// Execute the users command.
+			processTransfer.poll();
+
+		}
+
+		endTurn = false;
+
+		// Check challenge completion.
+		checkChallenges(player);
+
+	}
+
+	/**
+	 * Creates an {@link Action} that displays the turn of the specified
+	 * {@link Player} on the {@link UserInterface}. This {@link Action} is then
+	 * passed to the {@link UserInterface} thread while this thread waits for it to
+	 * be completed using {@link Action#isDone()}.
+	 * 
+	 * @param player
+	 *            {@link Player}
+	 */
+	private void displayTurn(Player player) {
+
+		// Display the turn to the user.
+		Action<UserInterface> action = new Action<>(ui, ui -> ui.displayTurn(player));
+
+		// Transfer the action to the ui.
+		processTransfer.transfer(action);
+
+		// Wait for the ui to display the turn to the player.
+		while (!action.isDone() && run) {
+			run();
+		}
+	}
+
+	/**
+	 * Iterates thought all the available {@link Challenge}s to see if the specified
+	 * {@link Player} has completed them or not.
+	 * 
+	 * @param currentPlayer
+	 *            {@link Player}
+	 */
+	private void checkChallenges(Player currentPlayer) {
+
+		// Holds the completed challenges
+		List<Challenge> toRemove = new LinkedList<>();
+
+		// Iterate though all the objectives to see if the the current player has
+		// completed them.
+		for (Challenge challenge : challenges) {
+
+			// If the current player has completed the challenge remove it from the list of
+			// available challenges.
+			if (challenge.hasCompleted(currentPlayer, board)) {
+				toRemove.add(challenge);
+			}
+		}
+
+		// Remove the completed challenges.
+		toRemove.forEach(challenge -> challenges.remove(challenge));
+	}
+
 	/*
 	 * Performs all the tasks that occur at the end of a round.
 	 */
 	private void endRound() {
-		// TODO end round operations.
 
 		board.endRound();
 
@@ -219,12 +305,8 @@ public class Game {
 
 		// Create the instance of the game.
 		Game game = new Game();
+		game.play();
 
-		// Create the instance of the user interface.
-		UserInterface ui = new UserInterface(game);
-
-		// Start the background thread.
-		game.run();
 	}
 
 }

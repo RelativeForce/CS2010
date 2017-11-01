04909cbbe8f4153f20e000a29b649032332308af
Joseph Rolli <rollij@aston.ac.uk>
2017-11-01 21:25:20 +0000

    Implementing drawing of armies, and documentation.

--- CS2010/src/peril/ui/states/CoreGameState.java
+++ CS2010/src/peril/ui/states/CoreGameState.java
@@ -1,12 +1,16 @@
 package peril.ui.states;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import peril.Game;
+import peril.board.Continent;
 import peril.board.Country;
 
 /**
@@ -29,6 +33,8 @@ public abstract class CoreGameState extends BasicGameState {
 		this.game = game;
 		this.higlightedCountry = null;
 	}
+	
+	
 
 	public void highlight(Country country) {
 		higlightedCountry = country;
@@ -40,6 +46,10 @@ public abstract class CoreGameState extends BasicGameState {
 	}
 
 	@Override
+	/**
+	 * Called when the state is first entered, before slick2d's game loop commences. Initialises the state and loads resources.
+	 * 
+	 */
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		gc.setUpdateOnlyWhenVisible(true);
 
@@ -49,6 +59,10 @@ public abstract class CoreGameState extends BasicGameState {
 	}
 
 	@Override
+	/**
+	 * Called as part of slick2d's game loop. Renders this state to the game's graphics context 
+	 * 
+	 */
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 
 		if (game.getBoard().hasImage())
@@ -64,18 +78,58 @@ public abstract class CoreGameState extends BasicGameState {
 
 		g.drawString(stateName, 5, 50);
 		g.drawString("X" + x + " Y" + y, 700, 700);
+		drawArmies(g);
 	}
 
 	@Override
+	/**
+	 * Called as part of slick2d's game loop. Update the state's logic based on the amount of time that has passed.
+	 * 
+	 */
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		if (gc.getInput().isMouseButtonDown(0)) {
 			x = gc.getInput().getMouseX();
 			y = gc.getInput().getMouseY();
 		}
 	}
-
+	
+	/**
+	 * Returns the name of the current state, as a String.
+	 * 
+	 */
 	public String getStateName() {
 		return stateName;
+	}
+
+	
+	/**
+	 * Draws the {@link army} in its current state over the {@link Country} it is located.
+	 * 
+	 */
+	public void drawArmies(Graphics g) {
+		
+		//Iterate across every country in every continent on the game board.
+		for (Continent continent : game.getBoard().getContinents()) {
+			for (Country country : continent.getCountries()) {
+				
+				//Sets x and y as the central width and height of the current country. 
+				int x = country.getPosition().x + country.getWidth()/2;
+				int y =	country.getPosition().y + country.getHeight()/2;
+				
+				//Draw a background oval.
+				g.setColor(Color.lightGray);
+				g.fillOval(x, y, 15, 25);
+				
+				g.setColor(Color.black);
+				//If the country has an army, draw a string representing the number of troops within that army at (x,y).
+				if (country.getArmy() != null) {
+					int troopNumber = country.getArmy().getSize();
+					g.drawString(Integer.toString(troopNumber), x, y);
+				}else {
+					g.drawString(Integer.toString(0), x, y);
+				}
+			}
+		}
 	}
 
 }

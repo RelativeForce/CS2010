692e3a513f59bf91957a3c200801df53427ac7b2
Joseph Rolli <rollij@aston.ac.uk>
2017-11-02 17:24:33 +0000

    Added button class.

--- CS2010/src/peril/ui/Button.java
+++ CS2010/src/peril/ui/Button.java
@@ -1,12 +1,15 @@
 package peril.ui;
 
+import org.newdawn.slick.Image;
+
+import peril.Point;
 import peril.ui.visual.Clickable;
 
 public class Button extends Clickable {
 	
 	
-	Button(){
+	Button(Point position, Image image){
 		super();
-		
+		this.setImage(position, image);
 	}
 }

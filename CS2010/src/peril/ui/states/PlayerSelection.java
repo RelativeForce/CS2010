19da006710ff4874a3406f427029aa1e47facaba
RelativeForce <joshiceddy@hotmail.com>
2017-12-18 21:58:54 +0000

    Documented PlayerSelection state

--- CS2010/src/peril/ui/states/PlayerSelection.java
+++ CS2010/src/peril/ui/states/PlayerSelection.java
@@ -22,14 +22,35 @@ import peril.ui.Font;
 import peril.ui.components.lists.VisualList;
 import peril.ui.components.menus.HelpMenu;
 
+/**
+ * The {@link InteractiveState} that allows the user to select the number of
+ * {@link Player}s and the {@link AI} that control those {@link Player}s.
+ * 
+ * @author Joshua_Eddy
+ *
+ */
 public class PlayerSelection extends InteractiveState {
 
+	/**
+	 * The name of this {@link PlayerSelection}.
+	 */
 	private static final String NAME = "Select";
 
+	/**
+	 * The {@link Map} that contains all the {@link AI} that are available for the
+	 * user to select.
+	 */
 	private final Map<String, AI> allAI;
 
+	/**
+	 * The {@link Map} of {@link Player} number to there associated
+	 * {@link VisualList} that selects the {@link AI} for that player.
+	 */
 	private final Map<Integer, VisualList<AI>> selectors;
 
+	/**
+	 * The string id of the play button.
+	 */
 	private final String playButton;
 
 	/**
@@ -38,6 +59,14 @@ public class PlayerSelection extends InteractiveState {
 	 */
 	private VisualList<Integer> players;
 
+	/**
+	 * Constructs a new {@link PlayerSelection}.
+	 * 
+	 * @param game
+	 *            {@link Game} this {@link PlayerSelection} is a part of.
+	 * @param id
+	 *            The id of this {@link PlayerSelection}.
+	 */
 	public PlayerSelection(Game game, int id) {
 		super(game, NAME, id, HelpMenu.NULL_PAGE);
 
@@ -74,15 +103,20 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Processes a click at a specified {@link Point} position on this
+	 * {@link PlayerHelper}.
+	 */
 	@Override
 	public void parseClick(int button, Point click) {
 
 		if (!super.clickedButton(click)) {
+
+			// If they click the players list is clicked then configure the selectors.
 			if (players.click(click)) {
-
 				confingureSelectors();
-
 			} else {
+
 				for (VisualList<AI> selector : selectors.values()) {
 
 					if (selector.click(click)) {
@@ -95,12 +129,18 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Processes a button press on this {@link PlayerSelection}.
+	 */
 	@Override
 	public void parseButton(int key, char c, Point mousePosition) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Renders this {@link PlayerSelection}.
+	 */
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 
@@ -129,33 +169,44 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Retrieves the {@link Music} that plays during {@link PlayerSelection}.
+	 */
 	@Override
 	public Music getMusic() {
 		return null;
 	}
 
+	/**
+	 * Load the game based on the current state of the elements in this
+	 * {@link PlayerSelection}.
+	 */
 	public void loadGame() {
 
+		// Iterate through the number of players the player has selected
 		for (int index = 1; index <= players.getSelected(); index++) {
 
-			// The color assigned to that player.
+			// The colour assigned to that player.
 			Color color = getGame().players.getColor(index);
 
 			// Set the player with the AI that the user selected.
 			Player player = new Player(index, color, selectors.get(index).getSelected());
 
-			// The player Icon for that player.
-			Image playerIcon = getGame().players.getPlayerIcon(index);
-
-			player.setImage(new Point(100, 100), playerIcon);
+			// Set the player Icon for that player.
+			player.setImage(new Point(100, 100), getGame().players.getPlayerIcon(index));
 
 			getGame().players.addPlayer(player);
 
 		}
 
+		// Load the game
 		getGame().enterState(getGame().states.loadingScreen);
 	}
 
+	/**
+	 * Adds the {@link VisualList} selectors to {@link PlayerSelection#selectors}
+	 * that allow the user to select which {@link AI} {@link Player}s have.
+	 */
 	private void addSelectors() {
 
 		for (int index = 1; index <= PlayerHelper.MAX_PLAYERS; index++) {
@@ -172,6 +223,10 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Configures which {@link PlayerSelection#selectors} are visible based on the
+	 * selected value of {@link PlayerSelection#players}.
+	 */
 	private void confingureSelectors() {
 
 		selectors.forEach((playerNumber, selector) -> {
@@ -186,10 +241,28 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Sets the position of the {@link PlayerSelection#selectors} based on the
+	 * dimensions of the specified {@link GameContainer}.
+	 * 
+	 * @param gc
+	 *            {@link GameContainer}
+	 */
 	private void positionSelectors(GameContainer gc) {
 		selectors.forEach((playerNumber, selector) -> selector.setPosition(getSelectorPosition(playerNumber, gc)));
 	}
 
+	/**
+	 * Retrieves the {@link Point} position of a {@link PlayerSelection#selectors}
+	 * element based on the specified {@link Player} number and the
+	 * {@link GameContainer}.
+	 * 
+	 * @param playerNumber
+	 *            {@link Player} number
+	 * @param gc
+	 *            {@link GameContainer}
+	 * @return {@link Point}
+	 */
 	private Point getSelectorPosition(int playerNumber, GameContainer gc) {
 
 		int width = gc.getWidth();
@@ -202,10 +275,20 @@ public class PlayerSelection extends InteractiveState {
 
 	}
 
+	/**
+	 * Populates the {@link VisualList} selector with all the elements of
+	 * {@link PlayerSelection#allAI}.
+	 * 
+	 * @param selector
+	 *            {@link VisualList} selector.
+	 */
 	private void populateSelector(VisualList<AI> selector) {
 		allAI.forEach((AIName, AI) -> selector.add(AIName, AI));
 	}
 
+	/**
+	 * Defines all the {@link AI} that are available for the user to select.
+	 */
 	private void addAllAI() {
 		allAI.put("Monkey", new Monkey(getGame().api));
 		allAI.put("None", AI.USER);

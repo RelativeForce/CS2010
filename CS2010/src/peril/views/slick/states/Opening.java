6a060b95e8e1adfadda91cd55a47384d283897b1
RelativeForce <joshiceddy@hotmail.com>
2018-02-13 14:59:46 +0000

    Added Opening Splash

--- CS2010/src/peril/views/slick/states/Opening.java
+++ CS2010/src/peril/views/slick/states/Opening.java
@@ -1,10 +1,13 @@
 package peril.views.slick.states;
 
-import org.newdawn.slick.Animation;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.StateBasedGame;
 
 import peril.controllers.GameController;
@@ -17,45 +20,38 @@ public class Opening extends InteractiveState {
 
 	private static final String NAME = "Opening";
 
-	private Splash splash;
+	private int index;
+
+	private final List<SplashState> states;
 
 	public Opening(GameController game, int id) {
 		super(game, NAME, id, HelpMenu.NULL_PAGE);
+
+		index = 0;
+
+		states = new LinkedList<>();
+
+		addStates();
+	}
+
+	private void addStates() {
+		states.add(new SpinningGoat());
+		states.add(new Bah());
 	}
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		super.init(gc, sbg);
 
-		final int frameRate = 1000 / 30;
+		states.forEach(state -> {
+			try {
+				state.init(gc);
+			} catch (SlickException e) {
+				e.printStackTrace();
 
-		final String splashPath = game.getDirectory().getSplashPath();
-
-		this.splash = new Splash();
-		Image frame = null;
-		int index = 0;
-
-		// Iterate over the frames of the splash screen until no frame is found.
-		do {
-
-			frame = ImageReader.getImage(splashPath + index + ".png");
-
-			if (frame != null) {
-
-				// Scale the frame
-				frame = frame.getScaledCopy(gc.getScreenWidth(), gc.getScreenHeight());
-
-				// Add the frame to the splash screen.
-				splash.addFrame(frame, frameRate);
-
-				index++;
 			}
 
-			// If frame is null then there are no more frames.
-		} while (frame != null);
-
-		// Set the splash to finish at the final frame.
-		splash.stopAt(index);
+		});
 
 	}
 
@@ -66,16 +62,26 @@ public class Opening extends InteractiveState {
 
 	@Override
 	public void render(GameContainer gc, Frame frame) {
-		// splash.draw();
+		states.get(index).draw(frame);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		super.update(gc, sbg, delta);
 
-		// If the splash page has finished enter the main menu
-		if (splash.isStopped()) {
-			game.getView().enterMainMenu();
+		SplashState current = states.get(index);
+
+		if (current.isFinished()) {
+
+			if (index == states.size() - 1) {
+				slick.enterMainMenu();
+			} else {
+				index++;
+			}
+
+		} else {
+			current.update(gc);
+
 		}
 
 	}
@@ -85,19 +91,145 @@ public class Opening extends InteractiveState {
 		return null;
 	}
 
-	private final class Splash extends Animation {
+	interface SplashState {
 
-		@Override
-		protected final void finalize() throws Throwable {
+		void draw(Frame g);
 
-			// Destroy all the images in the splash screen.
-			for (int index = 0; index < getFrameCount(); index++) {
-				getImage(index).destroy();
-			}
+		void init(GameContainer gc) throws SlickException;
 
-			super.finalize();
-		}
+		void update(GameContainer gc);
+
+		boolean isFinished();
 
 	}
 
+	private final class SpinningGoat implements SplashState {
+
+		private final static int NUMBER_OF_ROTATIONS = 3;
+
+		private final static int NUMBER_OF_FRAMES = 120;
+
+		private Image goat;
+
+		private float goatX;
+
+		private float goatY;
+
+		private float rotationSpeed;
+
+		private float maxY;
+
+		private float fallingSpeed;
+
+		private boolean isFinished;
+
+		public SpinningGoat() {
+			isFinished = false;
+		}
+
+		@Override
+		public void draw(Frame frame) {
+			frame.draw(goat, (int) goatX, (int) goatY);
+		}
+
+		@Override
+		public void init(GameContainer gc) {
+
+			goat = ImageReader.getImage(game.getDirectory().getSplashPath()+ "goat.png");
+
+			final float goatScale = (float) (gc.getWidth() / 3) / goat.getWidth();
+
+			goat = goat.getScaledCopy(goatScale);
+
+			goatX = (gc.getWidth() - goat.getWidth()) / 2;
+			goatY = -goat.getHeight();
+
+			maxY = gc.getHeight() + goat.getHeight();
+
+			fallingSpeed = (maxY - goatY) / NUMBER_OF_FRAMES;
+
+			rotationSpeed = (360f / NUMBER_OF_FRAMES) * NUMBER_OF_ROTATIONS;
+
+		}
+
+		@Override
+		public void update(GameContainer gc) {
+			goat.rotate(rotationSpeed);
+
+			goatY += fallingSpeed;
+
+			if (goatY >= maxY) {
+				isFinished = true;
+			}
+		}
+
+		@Override
+		public boolean isFinished() {
+			return isFinished;
+		}
+	}
+
+	private final class Bah implements SplashState {
+
+		private Sound bah;
+
+		private boolean isFinished;
+
+		public Bah() {
+			isFinished = false;
+		}
+
+		@Override
+		public void draw(Frame g) {
+			// Draw nothing
+		}
+
+		@Override
+		public void init(GameContainer gc) throws SlickException {
+			bah = new Sound(game.getDirectory().getMusicPath() + "bah.ogg");
+		}
+
+		@Override
+		public void update(GameContainer gc) {
+			bah.play();
+
+			isFinished = true;
+		}
+
+		@Override
+		public boolean isFinished() {
+			return isFinished;
+		}
+	}
+
+	private final class TitleCard implements SplashState {
+
+		private boolean isFinished;
+
+		public TitleCard() {
+			isFinished = true;
+		}
+
+		@Override
+		public void draw(Frame g) {
+
+		}
+
+		@Override
+		public void init(GameContainer gc) throws SlickException {
+
+		}
+
+		@Override
+		public void update(GameContainer gc) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public boolean isFinished() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
 }

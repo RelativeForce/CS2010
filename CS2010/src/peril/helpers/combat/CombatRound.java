e57dc5e1f08b140f80a08a26bc82e3520b0b91e9
RelativeForce <joshiceddy@hotmail.com>
2018-02-22 22:22:08 +0000

    Documented CombatRound

--- CS2010/src/peril/helpers/combat/CombatRound.java
+++ CS2010/src/peril/helpers/combat/CombatRound.java
@@ -2,13 +2,53 @@ package peril.helpers.combat;
 
 import peril.model.board.ModelCountry;
 
+/**
+ * Denotes a round of combat that can be processed by the {@link CombatHelper}.
+ * 
+ * @author Joshua_Eddy
+ * 
+ * @since 2018-02-22
+ * @version 1.01.01
+ * 
+ * @see CombatHelper
+ * @see ModelSquad
+ * @see ModelCountry
+ *
+ */
 public class CombatRound {
 
+	/**
+	 * The {@link ModelCountry} that will attack the {@link #defending}
+	 * {@link ModelCountry}.
+	 */
 	public final ModelCountry attacking;
+
+	/**
+	 * The {@link ModelCountry} that will defend against the {@link #attacking}
+	 * {@link ModelCountry}.
+	 */
 	public final ModelCountry defending;
+
+	/**
+	 * The {@link ModelSquad} that will attack the {@link #defenderSquad}.
+	 */
 	public final ModelSquad attackerSquad;
+
+	/**
+	 * The {@link ModelSquad} that will defend against the {@link #defenderSquad}.
+	 */
 	public final ModelSquad defenderSquad;
 
+	/**
+	 * Constructs a new {@link CombatRound}.
+	 * 
+	 * @param attacking
+	 *            The {@link ModelCountry} that will attack the {@link #defending}
+	 *            {@link ModelCountry}.
+	 * @param defending
+	 *            The {@link ModelCountry} that will defend against the
+	 *            {@link #attacking} {@link ModelCountry}.
+	 */
 	public CombatRound(ModelCountry attacking, ModelCountry defending) {
 
 		this.attacking = attacking;

838f2384b6352af15aef4ab1287bc303dcd05877
RelativeForce <joshiceddy@hotmail.com>
2018-02-25 12:57:22 +0000

    Documented AIOperation

--- CS2010/src/peril/ai/AIOperation.java
+++ CS2010/src/peril/ai/AIOperation.java
@@ -3,17 +3,42 @@ package peril.ai;
 import java.util.LinkedList;
 import java.util.List;
 
+import peril.controllers.AIController;
 import peril.controllers.api.Country;
 
-public class AIOperation {
+/**
+ * 
+ * Defines a instruction for the {@link AIController} to execute on the
+ * graphical thread.
+ * 
+ * @author Joshua_Eddy
+ * 
+ * @since 2018-02-25
+ * @version 1.01.01
+ * 
+ * @see AI
+ *
+ */
+public final class AIOperation {
 
+	/**
+	 * Whether the {@link AI} should perform another operation or move to the next
+	 * state.
+	 */
 	public boolean processAgain;
-	
+
+	/**
+	 * The {@link Country}'s the {@link AI} has specified to be selected in the
+	 * order they appear in the list. Index 0 first.
+	 */
 	public final List<Country> select;
-	
+
+	/**
+	 * Constructs a new {@link AIOperation}.
+	 */
 	public AIOperation() {
 		this.select = new LinkedList<>();
 		this.processAgain = false;
 	}
-	
+
 }

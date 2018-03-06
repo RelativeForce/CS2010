01dacb8b1d0d2afe9c3cb9c5965fc25a074f5406
RelativeForce <joshiceddy@hotmail.com>
2018-03-06 11:53:52 +0000

    Documented Point Helper

--- CS2010/src/peril/helpers/PointHelper.java
+++ CS2010/src/peril/helpers/PointHelper.java
@@ -3,13 +3,13 @@ package peril.helpers;
 import peril.controllers.Points;
 
 /**
- * Provides the point reward amounts for in-game actions. Realises
- * {@link Points}.
+ * Provides the point reward and cost amounts for in-game transactions..
+ * Realises {@link Points}.
  * 
- * @author Joseph_Rolli, Joshua Eddy
+ * @author Joseph_Rolli, Joshua_Eddy
  * 
- * @since 2018-02-21
- * @version 1.01.03
+ * @since 2018-03-06
+ * @version 1.01.04
  * 
  * @see Points
  *
@@ -17,16 +17,26 @@ import peril.controllers.Points;
 
 public final class PointHelper implements Points {
 
-	private static final PointHelper INSTANCE = new PointHelper();
-
 	/**
-	 * Points rewarded for conquering a country.
+	 * The point reward for conquering a country.
 	 */
 	public static final int CONQUER_REWARD = 2;
 
+	/**
+	 * The point cost for blockading a link between two countries.
+	 */
 	public static final int BLOCKADE_COST = 4;
 
+	/**
+	 * The point cost for trading a set of units for the unit directly above that
+	 * set of units in terms of strength.
+	 */
 	public static final int TRADE_UNIT_COST = 3;
+
+	/**
+	 * The singleton instance of the {@link PointHelper}.
+	 */
+	private static final PointHelper INSTANCE = new PointHelper();
 
 	/**
 	 * Constructs a new {@link PointHelper}.
@@ -35,6 +45,11 @@ public final class PointHelper implements Points {
 		// Do nothing
 	}
 
+	/**
+	 * Retrieves the singleton instance of {@link PointHelper}.
+	 * 
+	 * @return The {@link PointHelper}.
+	 */
 	public static PointHelper getInstance() {
 		return INSTANCE;
 	}

f1f50eb0a9dc4813b4db74c256361fb73c8015b2
RelativeForce <joshiceddy@hotmail.com>
2018-03-09 14:41:02 +0000

    Added CombatHelper test class

---
+++ CS2010/src-test/peril/model/states/combat/Test_CombatHelper.java
@@ -0,0 +1,24 @@
+package peril.model.states.combat;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test_CombatHelper {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}

f8e5d969355647c4a8fe48905413c010878bb2f3
RelativeForce <joshiceddy@hotmail.com>
2018-03-11 19:50:16 +0000

    Added Test for dice roll order

--- CS2010/src-test/peril/model/states/combat/Test_CombatHelper.java
+++ CS2010/src-test/peril/model/states/combat/Test_CombatHelper.java
@@ -8,16 +8,20 @@ import java.util.function.Consumer;
 import org.junit.Before;
 import org.junit.Test;
 import peril.Challenge;
+import peril.ai.AI;
 import peril.controllers.AIController;
 import peril.controllers.Directory;
 import peril.controllers.GameController;
 import peril.helpers.AIHelper;
 import peril.helpers.UnitHelper;
+import peril.model.ModelColor;
 import peril.model.ModelPlayer;
 import peril.model.board.ModelArmy;
 import peril.model.board.ModelBoard;
 import peril.model.board.ModelCountry;
 import peril.model.board.ModelUnit;
+import peril.model.board.links.ModelLink;
+import peril.model.board.links.ModelLinkState;
 import peril.model.states.*;
 import peril.views.View;
 
@@ -27,7 +31,7 @@ import peril.views.View;
  * 
  * @author Joshua_Eddy
  * 
- * @version 1.01.01
+ * @version 1.01.03
  * @since 2018-03-11
  * 
  * @see CombatHelper
@@ -73,15 +77,95 @@ public final class Test_CombatHelper {
 		this.testArmyUnits.add(testUnit1);
 
 	}
-	
+
+	/**
+	 * Test that the dice that are in {@link CombatView#attackerDiceRolls} and
+	 * {@link CombatView#defenderDiceRolls} are ordered from highest (index: 0) to
+	 * lowest.
+	 */
 	@Test
-	public void test() {
-		
+	public void test_diceOrder() {
+
 		combat.clear();
-		
-		assertTrue(true);
-		
-		
+
+		// Constructs the combat round with no exceptions.
+		combat.fight(getStandardRound());
+
+		// Holds the post view combat view.
+		final CombatView view = combat.view;
+
+		// Check the dice rolls.
+		checkDiceRolls(view.attackerDiceRolls);
+		checkDiceRolls(view.defenderDiceRolls);
+
+	}
+
+	/**
+	 * Checks that the numbers that are in the parameter dice rolls are in
+	 * descending order.
+	 * 
+	 * @param diceRolls
+	 *            The dice rolls.
+	 */
+	private void checkDiceRolls(Integer[] diceRolls) {
+
+		// The current lowest dice that has been seen
+		int currentLowest = 0;
+
+		// Iterate over the dice rolls.
+		for (int attackIndex = 0; attackIndex < diceRolls.length; attackIndex++) {
+
+			// If this is the first dice then set it as the current lowest.
+			if (attackIndex == 0) {
+				currentLowest = diceRolls[attackIndex];
+			} else {
+
+				assertTrue(currentLowest >= diceRolls[attackIndex]);
+				currentLowest = diceRolls[attackIndex];
+			}
+
+		}
+
+	}
+
+	/**
+	 * Constructs a standard {@link CombatRound} for testing purposes.
+	 * 
+	 * @return A standard {@link CombatRound}
+	 */
+	private CombatRound getStandardRound() {
+
+		// Define the players that will rule both countries.
+		final ModelPlayer player1 = new ModelPlayer(1, AI.USER);
+		final ModelPlayer player2 = new ModelPlayer(2, AI.USER);
+
+		// The countries taking part in the war.
+		final ModelCountry country1 = new ModelCountry("country1", new ModelColor(0, 0, 0));
+		final ModelCountry country2 = new ModelCountry("country2", new ModelColor(0, 0, 1));
+
+		// Set the countries as linked.
+		country1.addNeighbour(country2, new ModelLink(ModelLinkState.OPEN));
+		country2.addNeighbour(country1, new ModelLink(ModelLinkState.OPEN));
+
+		// Assign the rulers.
+		country1.setRuler(player1);
+		country2.setRuler(player2);
+
+		// Build the armies
+		country1.getArmy().add(testArmyUnits);
+		country2.getArmy().add(testArmyUnits);
+
+		// Define the squads
+		final ModelSquad attackSquad = new ModelSquad(CombatHelper.MAX_ATTACK_SQUAD_SIZE);
+		final ModelSquad defendSquad = new ModelSquad(CombatHelper.MAX_DEFEND_SQUAD_SIZE);
+
+		// Auto populate the squads
+		attackSquad.autoPopulate(country1.getArmy(), 1);
+		defendSquad.autoPopulate(country2.getArmy(), 0);
+
+		// Constructs the combat round with no exceptions.
+		return new CombatRound(country1, country2, attackSquad, defendSquad);
+
 	}
 
 	/**

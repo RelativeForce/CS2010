3a2c13e3295346f1448745676950e4963d98fa4a
RelativeForce <joshiceddy@hotmail.com>
2018-03-11 19:22:46 +0000

    Moved param checks into methods

--- CS2010/src/peril/model/states/combat/CombatRound.java
+++ CS2010/src/peril/model/states/combat/CombatRound.java
@@ -8,7 +8,7 @@ import peril.model.board.ModelCountry;
  * @author Joshua_Eddy
  * 
  * @since 2018-03-11
- * @version 1.01.03
+ * @version 1.01.04
  * 
  * @see CombatHelper
  * @see ModelSquad
@@ -43,17 +43,7 @@ public final class CombatRound {
 	 * Constructs a new {@link CombatRound}.<br>
 	 * <br>
 	 * 
-	 * Key Aspects:
-	 * <ul>
-	 * <li>The attacker and defender must be ruled by different players.</li>
-	 * <li>The attacker and defender must be neighbours with a valid link between
-	 * them that allows an attack.</li>
-	 * <li>The attacker and defender must be different countries.</li>
-	 * <li>The attacker and defender squads must be different squads.</li>
-	 * <li>The attacker and defender squads must have a valid number of alive units
-	 * in them.</li>
-	 * <li>The attacker and defender squads must not be empty.</li>
-	 * </ul>
+	 * 
 	 * 
 	 * @param attacker
 	 *            The {@link ModelCountry} that will attack the {@link #defender}
@@ -70,6 +60,33 @@ public final class CombatRound {
 	 */
 	public CombatRound(ModelCountry attacker, ModelCountry defender, ModelSquad attackerSquad,
 			ModelSquad defenderSquad) {
+
+		this.attacker = attacker;
+		this.defender = defender;
+		this.attackerSquad = attackerSquad;
+		this.defenderSquad = defenderSquad;
+
+		checkState();
+	}
+
+	/**
+	 * Checks that the following rules still remain true for the state of this
+	 * {@link CombatRound}. If there is anything about this {@link CombatRound} that
+	 * is not valid a appropriate exception will be thrown.
+	 * 
+	 * Key Aspects:
+	 * <ul>
+	 * <li>The attacker and defender must be ruled by different players.</li>
+	 * <li>The attacker and defender must be neighbours with a valid link between
+	 * them that allows an attack.</li>
+	 * <li>The attacker and defender must be different countries.</li>
+	 * <li>The attacker and defender squads must be different squads.</li>
+	 * <li>The attacker and defender squads must have a valid number of alive units
+	 * in them.</li>
+	 * <li>The attacker and defender squads must not be empty.</li>
+	 * </ul>
+	 */
+	public void checkState() {
 
 		// Check the countries aren't ruled by the same player.
 		if (attacker.getRuler().equals(defender.getRuler())) {
@@ -98,10 +115,5 @@ public final class CombatRound {
 			throw new IllegalArgumentException(defendSquadSize + " is not a valid defending squad size.");
 		}
 
-		this.attacker = attacker;
-		this.defender = defender;
-		this.attackerSquad = attackerSquad;
-		this.defenderSquad = defenderSquad;
 	}
-
 }

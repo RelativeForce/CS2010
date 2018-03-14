a1dcdb937ad03f72ed80dea74fc033c6c2b2e145
adrianw17 <adrian_ftw@outlook.com>
2018-03-14 14:46:07 +0000

    changed the help and challenges menu

--- CS2010/src-test/peril/model/board/Test_ModelHazard.java
+++ CS2010/src-test/peril/model/board/Test_ModelHazard.java
@@ -2,23 +2,43 @@ package peril.model.board;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class tests {@link ModelHazard}.
+ * 
+ * @author Adrian_Wong
+ *
+ */
+
 public class Test_ModelHazard {
+
+	private String VOLCANIC_ERUPTION;
+	
+	private String TORNADO;
+	
+	private String HURRICANE;
+	
+	private String TSUNAMI;
+	
 
 	@Before
 	public void setUp() throws Exception {
-	}
 
-	@After
-	public void tearDown() throws Exception {
+		VOLCANIC_ERUPTION = "Volcanic Eruption";
+		TORNADO = "Tornado";
+		HURRICANE = "Hurricane";
+		TSUNAMI = "Tsunami";;
+					
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_getByName() {
+		assertEquals(VOLCANIC_ERUPTION, ModelHazard.VOLCANIC_ERUPTION.toString());
+		assertEquals(TORNADO, ModelHazard.TORNADO.toString());
+		assertEquals(HURRICANE, ModelHazard.HURRICANE.toString());
+		assertEquals(TSUNAMI, ModelHazard.TSUNAMI.toString());
 	}
 
 }
